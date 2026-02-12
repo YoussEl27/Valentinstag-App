829cab26729adbc90ffb07ec922fed800a8aca8e
YoussEl27 <elkrttaniysf15@gmail.com>
2026-02-12 23:56:38 +0100

    "Update base path for deployment"

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 function Router() {
   return (
     <Switch>
-      <Route path="/" component={Home} />
+      <Route path="/Valentinstag-App/" component={Home} />
       <Route component={NotFound} />
     </Switch>
   );
